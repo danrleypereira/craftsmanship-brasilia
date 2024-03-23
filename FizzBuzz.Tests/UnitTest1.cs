c24c4fe1cfec3116e880c0068fef5bfdd9e27d1d
Danrley Pereira <danrleywillian@gmail.com>
2024-03-23 14:46:24 -0300

    feat(FizzBuzz): tested and functional

---
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,94 @@
+using System.Reflection.PortableExecutable;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests;
+public class FizzBuzzSpecs
+{
+    [Fact]
+    public void isNumberOne()
+    {
+        // Arrange
+        var number = 1;
+        var fizzBuzz = new FizzBuzzCalculator();
+
+        //  Act
+        String[] result = fizzBuzz.returnResult(number);
+
+        // Assert
+        Assert.Equal(number.ToString(), result[0]);
+    }
+
+    [Fact]
+    public void isNumberTwoAndOne()
+    {
+        // Arrange
+        int number = 2;
+        // string[] expectedResult = new int[] { 1, 2 };
+        var fizzBuzz = new FizzBuzzCalculator();
+
+        //  Act
+        String[] result = fizzBuzz.returnResult(number);
+
+        // Assert
+        Assert.Equal("1", result[0]);
+        Assert.Equal("2", result[1]);
+    }
+
+    [Fact]
+    public void isFizz() {
+        // Arrange
+        var fizzBuzz = new FizzBuzzCalculator();
+        // Act
+        String[] result = fizzBuzz.returnResult(10);
+
+        // Assert
+        Assert.Equal("Fizz", result[2]);
+        Assert.Equal("Fizz", result[5]);
+        Assert.Equal("Fizz", result[8]);
+    }
+
+    [Fact]
+    public void isNumberFour()
+    {
+        // Arrange
+        var number = 4;
+        var fizzBuzz = new FizzBuzzCalculator();
+
+        //  Act
+        String[] result = fizzBuzz.returnResult(number);
+
+        // Assert
+        Assert.Equal(number.ToString(), result[3]);
+    }
+
+    [Fact]
+    public void isBuzz()
+    {
+        // Arrange
+        var fizzBuzz = new FizzBuzzCalculator();
+
+        //  Act
+        String[] result = fizzBuzz.returnResult(21);
+
+        // Assert
+        Assert.Equal("Buzz", result[4]);
+        Assert.Equal("Buzz", result[9]);
+        Assert.Equal("Buzz", result[19]);
+    }
+
+    [Fact]
+    public void isFizzBuzz()
+    {
+        // Arrange
+        var fizzBuzz = new FizzBuzzCalculator();
+
+        //  Act
+        String[] result = fizzBuzz.returnResult(90);
+
+        // Assert
+        Assert.Equal("FizzBuzz", result[14]);
+        Assert.Equal("FizzBuzz", result[29]);
+        Assert.Equal("FizzBuzz", result[44]);
+    }
+
+}
---
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzCalculator
+    {
+        public string[] returnResult(int width)
+        {
+            var strings = new string[100];
+            for(int remainingNumbers = width; remainingNumbers > 0; remainingNumbers--)
+            {
+                strings[remainingNumbers - 1] = check(remainingNumbers);
+            }
+            return strings;
+        }
+
+        private string check(int number) {
+            if ((number % 5 == 0) && (number % 3 == 0))
+                return "FizzBuzz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else
+                return number.ToString();
+        }
+    }
+}
